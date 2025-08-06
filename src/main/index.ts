c1b53956e9ed4d5947c1df639fe1c14892824614
gui <gmazzega2@gmail.com>
2025-08-06 13:18:47 -0300

    finalizacao

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,36 +8,64 @@ function sum(a: number, b: number) {
 function sub(a: number, b: number) {
     console.log(`Your result ${a - b}`);
 }
-
+function mult(a: number, b: number) {
+    console.log(`Your result: ${a * b}`);
+}
+function div(a:number, b:number){
+    console.log(`Your result: ${a / b}`)
+}
+function isNumber(a: number, b: number) {
+    if (isNaN(a) || isNaN(b)) {
+        console.log("One of the values is not a valid number");
+        return false;
+    } else {
+        return true;
+    }
+}
 let op: number;
 do {
     console.log(`
     ---TypeScript Calculator---
     1 - Sum
     2 - Subtraction
+    3 - Multiplication
+    4 - division
     0 - exit
     `);
     op = parseInt(prompt("Inform your operation"));
 
     if (op == 1) {
-        let n1: number = parseInt(prompt("Inform the first number!"));
-        let n2: number = parseInt(prompt("Inform the second number!"));
-        if (isNaN(n1) || isNaN(n2)) {
-            console.log("One of the values is not a valid number");
-        } else {
+        let n1: number = parseFloat(prompt("Inform the first number!"));
+        let n2: number = parseFloat(prompt("Inform the second number!"));
+
+        let testNumber: boolean = isNumber(n1, n2);
+        if (testNumber) {
             sum(n1, n2);
         }
-    }
-    if (op == 2) {
-        let n1: number = parseInt(prompt("Inform the first number!"));
-        let n2: number = parseInt(prompt("Inform the second number!"));
-        if (isNaN(n1) || isNaN(n2)) {
-            console.log("One of the values is not a valid number");
-        } else {
+    } else if (op == 2) {
+        let n1: number = parseFloat(prompt("Inform the first number!"));
+        let n2: number = parseFloat(prompt("Inform the second number!"));
+        let testNumber: boolean = isNumber(n1, n2);
+        if (testNumber) {
             sub(n1, n2);
         }
-    }
-    if (op == 0) {
-        console.log("bye");
+    } else if (op == 3) {
+        let n1: number = parseFloat(prompt("Inform the first number!"));
+        let n2: number = parseFloat(prompt("Inform the second number!"));
+        let testNumber: boolean = isNumber(n1, n2);
+        if (testNumber) {
+            mult(n1, n2);
+        }
+    } else if (op == 4) {
+        let n1: number = parseFloat(prompt("Inform the first number!"));
+        let n2: number = parseFloat(prompt("Inform the second number!"));
+        let testNumber: boolean = isNumber(n1, n2);
+        if (testNumber) {
+            div(n1, n2);
+        }
+    } else if (op == 0) {
+        console.log("Ending programn");
+    } else {
+        console.log("Invalid operation!");
     }
 } while (op != 0);
